3cda13d24ec91ba8d7d1650cf115ea689c7f147c
ProgrammerRimon <83152804+Rimon360@users.noreply.github.com>
2025-08-08 18:13:17 +0600

    init

--- backend/src/controllers/youtubeTagExtrac.js
+++ backend/src/controllers/youtubeTagExtrac.js
@@ -4,24 +4,14 @@ import * as cheerio from "cheerio";
 
 const youtubeTagExtractor = async (req, res) => {
   const { url } = req.body;
-  const normalizeYouTubeUrl = (url) => url.replace(/^https?:\/\/m\.youtube\.com/, "https://www.youtube.com");
   if (!url) return res.status(400).json({ error: "URL is required" });
 
   try {
-    const response = await axios.get(normalizeYouTubeUrl(url), {
-      headers: {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
-        "Accept-Language": "en-US,en;q=0.9",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-        "Referer": "https://www.youtube.com/"
-      },
-      timeout: 10000,
-    });
+    const response = await axios.get(url);
     const $ = cheerio.load(response.data);
-    console.log($("meta"));
+
     const tagsMeta = $('meta[name="keywords"]').attr("content");
-    const tags = tagsMeta ? tagsMeta.split(",").map((t) => t.trim()) : [];
-    console.log(tags);
+    const tags = ['We are comming soon...! we appreciate your patience']//tagsMeta ? tagsMeta.split(",").map((t) => t.trim()) : [];
 
     res.json({ tags });
   } catch (error) {
