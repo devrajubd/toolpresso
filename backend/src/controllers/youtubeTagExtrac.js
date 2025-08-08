6bf17da2460568d1847b3a350a86b77dfaba9511
ProgrammerRimon <83152804+Rimon360@users.noreply.github.com>
2025-08-08 17:49:35 +0600

    init

--- backend/src/controllers/youtubeTagExtrac.js
+++ backend/src/controllers/youtubeTagExtrac.js
@@ -18,7 +18,7 @@ const youtubeTagExtractor = async (req, res) => {
       timeout: 10000,
     });
     const $ = cheerio.load(response.data);
-    console.log($);
+    console.log($("meta"));
     const tagsMeta = $('meta[name="keywords"]').attr("content");
     const tags = tagsMeta ? tagsMeta.split(",").map((t) => t.trim()) : [];
     console.log(tags);
